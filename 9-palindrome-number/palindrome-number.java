f174da487e7d653bd5042dee360862490463d6e2
Jananimarappan2004 <150882133+Jananimarappan2004@users.noreply.github.com>
2024-08-16 21:34:08 +0530

    Time: 4 ms (100.00%) | Memory: 43.7 MB (86.84%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,16 +1,17 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x<0){
-            return false;
-        }
-        int rev=0;
-        int xcopy = x;
-        while(x>0){
-        int n=x%10;
+        int rem=0;
+        int xval=x;
+       if(x<0){
+        return false;
+       }
+       while(x>0){
+        int r=x%10;
+         rem=rem*10+r;
         x=x/10;
-        rev=rev*10+n;
-        }
-      
-    return rev==xcopy;
+       }
+        return rem==xval;
     }
+    
 }
+ 
