b786b4967a6a10500483fba557381c7d356e09f6
Jananimarappan2004 <150882133+Jananimarappan2004@users.noreply.github.com>
2024-08-17 12:08:47 +0530

    Time: 12 ms (8.44%) | Memory: 44.7 MB (6.65%) - LeetSync

--- 179-largest-number/largest-number.java
+++ 179-largest-number/largest-number.java
@@ -1,26 +1,27 @@
 class Solution {
     public String largestNumber(int[] nums) {
-        String str[]=new String[nums.length];
-        for(int i=0;i<nums.length;i++){
-            str[i]=String.valueOf(nums[i]);
+     String str[] = new String[nums.length];
+     for(int i=0;i<nums.length;i++){
+        str[i]=String.valueOf(nums[i]);
+     }
+     for(int i=0;i<str.length;i++){
+        for(int j=i+1;j<str.length;j++){
+            String order1 = str[i]+str[j];
+            String order2 = str[j]+str[i];
+          if(order1.compareTo(order2)<0){
+             String temp=str[i];
+             str[i]=str[j];
+             str[j]=temp;
+          }
         }
-        Arrays.sort(str,new Comparator<String>(){
-            @Override
-            public int compare(String a,String b){
-                String order1= a+b;
-                String order2 =b+a;
-            
-            return order2.compareTo(order1);
-            }
-        });
-        if(str[0].equals("0")){
-            return "0";
-        }
-        StringBuilder sb = new StringBuilder("");
-        for(String n:str){
-            sb.append(n);
-        }
-        return sb.toString();
-
-}
+     }
+     StringBuffer sb= new StringBuffer();
+     for(String num:str){
+        sb.append(num);
+     }
+    if(sb.charAt(0)=='0'){
+        return "0";
+    }
+      return sb.toString();
+    }
 }
