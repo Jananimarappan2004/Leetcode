f61542f846a562b4fbe683ff11e14428ade1119e
Jananimarappan2004 <150882133+Jananimarappan2004@users.noreply.github.com>
2024-08-19 15:05:58 +0530

    Time: 0 ms (100.00%) | Memory: 52.2 MB (5.05%) - LeetSync

--- 344-reverse-string/reverse-string.java
+++ 344-reverse-string/reverse-string.java
@@ -3,7 +3,8 @@ class Solution {
         char []res=new char[s.length];
         for(int i=s.length-1;i>=0;i--){
             res[s.length-i-1]=s[i];
+    
         }
-      System.arraycopy(res,0,s,0,s.length);
+     System.arraycopy(res,0,s,0,s.length);
 }
 }
