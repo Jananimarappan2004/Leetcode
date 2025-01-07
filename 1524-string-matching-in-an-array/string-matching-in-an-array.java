a40f7f684a04920212cd314a392b1a4fb2f18f11
Jananimarappan2004 <150882133+Jananimarappan2004@users.noreply.github.com>
2025-01-07 20:17:14 +0530

    Time: 5 ms (41.61%) | Memory: 42.5 MB (16.91%) - LeetSync

--- 1524-string-matching-in-an-array/string-matching-in-an-array.java
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.java
@@ -1,22 +1,16 @@
 class Solution {
     public List<String> stringMatching(String[] words) {
-         List<String> reslist = new ArrayList<>();
-        Set<String> resultSet = new HashSet<>();
-        
-        // Iterate through each word in the words array
-        for (int i = 0; i < words.length; i++) {
-            // Check against all other words to see if the current word is a substring of any
-            for (int j = 0; j < words.length; j++) {
-                // Skip comparing the word with itself
-                if (i != j && words[j].contains(words[i])) {
-                    resultSet.add(words[i]);
-                    break; // Once we find a match, no need to check further for this word
-                }
+          LinkedList<String> reslist = new LinkedList<>();
+          HashSet<String> set = new HashSet<>(); 
+          for(int i=0; i<words.length; i++){
+            for(int j=0; j<words.length; j++){
+            if(i!=j && (words[j].contains(words[i]))){
+               set.add(words[i]);
             }
         }
-        
-        // Convert the result set back to a list
-        reslist.addAll(resultSet);
+        }
+        reslist.addAll(set);
         return reslist;
+
     }
 }
