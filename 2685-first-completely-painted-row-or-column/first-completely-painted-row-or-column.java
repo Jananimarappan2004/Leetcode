c09ed5864580d1dc97f3017dcb82ffa450c2ffe8
Jananimarappan2004 <150882133+Jananimarappan2004@users.noreply.github.com>
2025-01-20 22:10:05 +0530

    Time: 3 ms (99.50%) | Memory: 57.6 MB (100.00%) - LeetSync

--- 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.java
+++ 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.java
@@ -1,48 +1,24 @@
 class Solution {
     public int firstCompleteIndex(int[] arr, int[][] mat) {
-        int numRows = mat.length, numCols = mat[0].length;
-        Map<Integer, Pair<Integer, Integer>> numToPos = new HashMap<>();
-        // Populate the numToPos map by iterating over the matrix
-        for (int row = 0; row < numRows; row++) {
-            for (int col = 0; col < numCols; col++) {
-                int value = mat[row][col];
-                numToPos.put(value, new Pair(row, col));
-            }
-        }
-
+         int[] map = new int[arr.length + 1];
         for (int i = 0; i < arr.length; i++) {
-            int num = arr[i];
-            Pair<Integer, Integer> pos = numToPos.get(num);
-            int row = pos.getKey();
-            int col = pos.getValue();
-            mat[row][col] = -mat[row][col]; // mark as seen
-
-            // Check if the row or column is completely painted
-            if (checkRow(row, mat) || checkColumn(col, mat)) {
-                return i;
-            }
+            map[arr[i]] = i;
         }
-
-        return -1; // This line will never be reached as per the problem statement
-    }
-
-    private boolean checkRow(int row, int[][] mat) {
-        // Return true if row is completely seen
-        for (int col = 0; col < mat[0].length; col++) {
-            if (mat[row][col] > 0) {
-                return false;
+        int ans = Integer.MAX_VALUE;
+        for (int i = 0; i < mat.length; i++) {
+            int max = 0;
+            for (int j = 0; j < mat[i].length; j++) {
+                max = Math.max(max, map[mat[i][j]]);
             }
+            ans = Math.min(ans, max);
         }
-        return true;
-    }
-
-    private boolean checkColumn(int col, int[][] mat) {
-        // Return true if col is completely seen
-        for (int row = 0; row < mat.length; row++) {
-            if (mat[row][col] > 0) {
-                return false;
+        for (int i = 0; i < mat[0].length; i++) {
+            int max = 0;
+            for (int j = 0; j < mat.length; j++) {
+                max = Math.max(max, map[mat[j][i]]);
             }
+            ans = Math.min(ans, max);
         }
-        return true;
+        return ans;
     }
 }
