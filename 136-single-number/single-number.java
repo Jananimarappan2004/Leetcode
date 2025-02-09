bacc2590b4eb1a90e5fc6f0a33193fe15aa7a6ae
Jananimarappan2004 <150882133+Jananimarappan2004@users.noreply.github.com>
2025-02-09 12:37:26 +0530

    Time: 512 ms (5.11%) | Memory: 46.6 MB (17.27%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,19 +1,19 @@
 class Solution {
     public int singleNumber(int[] nums) {
         int number = 0;
-        int count;
-        
-        for (int i = 0; i < nums.length; i++) {
-            count = 0;
-            for (int j = 0; j < nums.length; j++) {
-                if (nums[i] == nums[j]) {
-                    count++; // Count occurrences of nums[i]
+ 
+        for(int i=0; i<nums.length; i++){
+            int count=0;
+            for(int j = 0; j<nums.length; j++){
+                if(nums[i] == nums[j]){
+                  count++;
                 }
             }
-            if (count == 1) { // If it appears only once, return it
-                return nums[i];
+            if(count==1){
+                number = nums[i];
             }
         }
-        return number; // This will never execute for valid input
+         return number;
+        
     }
 }
